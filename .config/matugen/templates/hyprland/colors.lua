ba7a9d8eda79c44896b31d8a1b065cbebcca7280
MedhanshOO7 <Medhansh07@outlook.com>
2026-05-30 20:27:37 +0530

    SOme minor updated to the hyprland configs

--- .config/matugen/templates/hyprland/colors.lua
+++ .config/matugen/templates/hyprland/colors.lua
@@ -1,19 +1,16 @@
 hl.config({
     general = {
         col = {
-            active_border = {
-                colors = { "rgba({{colors.primary.default.hex_stripped}}cc)", "rgba({{colors.secondary.default.hex_stripped}}cc)" },
-                angle = 45
-            },
-            inactive_border = "rgba({{colors.outline.default.hex_stripped}}22)",
+            active_border = "0xff{{colors.primary.default.hex_stripped}}",
+            inactive_border = "0xff{{colors.outline.default.hex_stripped}}",
         },
     },
     misc = {
-        background_color = "rgba({{colors.surface.dark.hex_stripped}}FF)",
+        background_color = "0xff{{colors.surface_dim.default.hex_stripped}}",
     },
 })
 
 hl.window_rule({
-    match        = { pin = 1 },
-    border_color = "rgba({{colors.primary.default.hex_stripped}}AA) rgba({{colors.primary.default.hex_stripped}}77)",
+    match = { pin = 1 },
+    border_color = "0xff{{colors.primary.default.hex_stripped}}",
 })
