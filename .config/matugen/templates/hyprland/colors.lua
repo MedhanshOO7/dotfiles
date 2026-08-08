8d43577eddfff50b8848c0223c177639d1097d76
MedhanshOO7 <Medhansh07@outlook.com>
2026-08-08 14:02:13 +0530

    refactor(matugen): streamline configuration and remove unused templates

--- .config/matugen/templates/hyprland/colors.lua
+++ .config/matugen/templates/hyprland/colors.lua
@@ -1,16 +1,16 @@
 hl.config({
     general = {
         col = {
-            active_border = "0xff{{colors.primary.default.hex_stripped}}",
-            inactive_border = "0xff{{colors.outline.default.hex_stripped}}",
+            active_border   = "rgba({{colors.outline_variant.default.hex_stripped}}77)",
+            inactive_border = "rgba({{colors.surface_container_low.default.hex_stripped}}33)",
         },
     },
     misc = {
-        background_color = "0xff{{colors.surface_dim.default.hex_stripped}}",
+        background_color = "rgba({{colors.surface.dark.hex_stripped}}FF)",
     },
 })
 
 hl.window_rule({
-    match = { pin = 1 },
-    border_color = "0xff{{colors.primary.default.hex_stripped}}",
+    match        = { pin = 1 },
+    border_color = "rgba({{colors.primary.default.hex_stripped}}AA) rgba({{colors.primary.default.hex_stripped}}77)",
 })
